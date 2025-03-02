83bd44b064a0dd9cefbf4b4cfd64bdc18e8ad77c
thalassium <lucas.lambo@gmail.com>
2025-03-02 19:32:15 +0100

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/thalassium.github.io/", // Mets bien le nom du repo ici
   plugins: [vue()],
 })
